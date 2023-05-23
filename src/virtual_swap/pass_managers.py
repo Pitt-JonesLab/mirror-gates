d404039dc04bf44f85c35e8d07b223bc291d836e
Evan McKinney <evmckinney9@gmail.com>
2023-05-23 17:25:32 -0400

    setting up new benchmarks

---
+++ src/virtual_swap/pass_managers.py
@@ -0,0 +1,78 @@
+"""Pre-defined pass managers for benchmarking."""
+
+from qiskit import transpile
+from qiskit.transpiler.passes import (
+    ApplyLayout,
+    Collect2qBlocks,
+    ConsolidateBlocks,
+    EnlargeWithAncilla,
+    FullAncillaAllocation,
+    SabreLayout,
+    Unroller,
+)
+from qiskit.transpiler.passmanager import PassManager
+
+# this code is buggy, see https://github.com/Qiskit/qiskit-terra/pull/9375
+# I can't use this version bc qiskit version missing DAGCircuit functionality
+from slam.utils.transpiler_pass.weyl_decompose import RootiSwapWeylDecomposition
+
+from virtual_swap.sabre_swap import CNS_SabreSwap
+
+
+class CustomPassManager(PassManager):
+    def __init__(self, pm):
+        self.pm = pm
+
+        # force this ending, such that output is normalized
+        # we want all benchy PMs to count sqiswap gates
+        self.pm.append(
+            [
+                Collect2qBlocks(),
+                ConsolidateBlocks(force_consolidate=True),
+                RootiSwapWeylDecomposition(),
+            ]
+        )
+
+    def run(self, qc):
+        return self.pm.run(qc)
+
+
+class SabreCNS(CustomPassManager):
+    def __init__(self, coupling):
+        pm = PassManager()
+        routing = CNS_SabreSwap(coupling, heuristic="decay")
+        pm.append(SabreLayout(coupling, routing_pass=routing))
+        pm.append(
+            [FullAncillaAllocation(coupling), EnlargeWithAncilla(), ApplyLayout()]
+        )
+        pm.append(routing)
+        pm.append(Unroller(["u", "cx", "iswap", "swap"]))
+        super().__init__(pm)
+
+
+class Baseline(CustomPassManager):
+    def __init__(self, coupling):
+        pass
+        # NOTE, for some reason the StagedPassManager, created by level_3_pass_manager
+        # I cannot append my own pass to it
+        # config = PassManagerConfig(coupling_map=coupling, basis_gates=["cx", "u3"])
+        # self.pm = level_3_pass_manager(config)
+        # super().__init__(pm)
+
+    # override the run function, see NOTE above
+    def run(self, qc):
+        intermediate = transpile(
+            qc,
+            coupling_map=self.coupling,
+            basis_gates=["cx", "u3"],
+            optimization_level=3,
+        )
+        temp_pm = PassManager()
+        temp_pm.append(
+            [
+                Collect2qBlocks(),
+                ConsolidateBlocks(force_consolidate=True),
+                RootiSwapWeylDecomposition(),
+            ]
+        )
+        return temp_pm.run(intermediate)
