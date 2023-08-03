f5d8d2f33f73dfc8d707fc4185b9f9654857e3f6
Evan McKinney <evmckinney9@gmail.com>
2023-08-03 14:09:49 -0400

    full bench results

--- src/mirror_gates/logging.py
+++ src/mirror_gates/logging.py
@@ -3,8 +3,8 @@ import logging
 
 # Create a specific logger for transpile_benchy and set its level to DEBUG
 transpile_benchy_logger = logging.getLogger("transpile_benchy")
-# transpile_benchy_logger.setLevel(logging.INFO)
-transpile_benchy_logger.setLevel(logging.DEBUG)
+# transpile_benchy_logger.setLevel(logging.DEBUG)
+transpile_benchy_logger.setLevel(logging.INFO)
 # transpile_benchy_logger.setLevel(logging.WARNING)
 
 # Silence the qiskit logger by setting its level to WARNING or higher
